b0f5d406270d2b13ad2fa51e0cef1d2fded0c937
JM Ruiz <jmanuel.rar@emeraldigital.com>
2021-07-20 16:55:45 -0500

    test: Custom policies

---
+++ backend/api/activity/config/policies/isMan.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = async (ctx, next) => {
+  if (ctx.state.user.sex !== null) {
+    // Go to next policy or will reach the controller's action.
+    return await next();
+  }
+
+  ctx.unauthorized(`You're not allowed to perform this action, please define your sex!`);
+};
