651bba2e15300d4e3dab2f561952299b3a87e69f
JM Ruiz <jmanuel.rar@emeraldigital.com>
2021-07-20 17:16:50 -0500

    test: Custom Controller

--- backend/api/activity/controllers/activity.js
+++ backend/api/activity/controllers/activity.js
@@ -5,9 +5,57 @@
  * to customize this controller
  */
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 module.exports = {
   // GET /activities/hello
   async hello(ctx) {
     return 'Hello World!';
+  },
+
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    // TODO: Implement calculations to get the profit from collaborations
+    ctx.request.body.costActivity = 200.00
+
+    let entity = await strapi.services.activity.create(ctx.request.body);
+
+    return sanitizeEntity(entity, { model: strapi.models.activity });
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    // TODO: Implement calculations to get the profit from collaborations
+    ctx.request.body.costActivity = 500.00
+
+    let entity = await strapi.services.activity.update({ id }, ctx.request.body);
+
+    return sanitizeEntity(entity, { model: strapi.models.activity });
+  },
+
+  /**
+   * Delete a record.
+   *
+   * @return {Object}
+   */
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+    // TODO: Implement calculations, ammounts to update projectDuration
+
+    const entity = await strapi.services.activity.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.activity });
   },
 };
